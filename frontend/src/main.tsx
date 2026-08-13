3cd061c50c622ae97e643f6bf7c7e7fe8b7f7dec
AnonymousMickey <120690347+AnonymousMickey@users.noreply.github.com>
2026-08-13 09:26:51 +0530

    feat(main): bootstrap router and world provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
+import { WorldProvider } from './stores/WorldStore'
 import App from './App'
 import { ErrorBoundary } from './components/ErrorBoundary'
-import './index.css'
+import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <WorldProvider>
+            <App />
+          </WorldProvider>
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
   </StrictMode>,
 )
